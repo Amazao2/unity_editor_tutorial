fe1eb4c3011b0366f17faad01e8025a7cfaa8207
Brandon McRae <bmcrae2@uwo.ca>
2014-10-28 14:07:43 -0400

    Tutorial: The DrawDefaultInspectorFunction
    - adds SomeScript with variables in it
    - adds Editor script that overrides the SomeScript editor to use the
    default but also add a help box

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ unity_editor_tutorial
 CS4482: Tools1 - Learn the Unity Editor API
 
 First Tutorial Link: http://unity3d.com/learn/tutorials/modules/intermediate/editor/building-custom-inspector
+
+Second Tutorial Link: http://unity3d.com/learn/tutorials/modules/intermediate/editor/drawdefaultinspector-function
---
+++ editor_tutorial/Assets/Editor/SomeScriptEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(SomeScript))]
+public class SomeScriptEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector(); // display the default inspector since we only want to add to it.
+
+        EditorGUILayout.HelpBox("This is a help box", MessageType.Info);
+    }
+}
