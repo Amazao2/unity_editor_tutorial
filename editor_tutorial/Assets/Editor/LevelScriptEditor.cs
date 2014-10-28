52a5e13821172c88cc7ff724bdebfa2a9e92b77d
Brandon McRae <bmcrae2@uwo.ca>
2014-10-28 13:51:33 -0400

    Tutorial: Building a custom inspector
    - completed first tutorial
    - adds level script and editor script to change the appearance of this
    script
    - tested on the LevelTest gameobject

---
+++ editor_tutorial/Assets/Editor/LevelScriptEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelScript))] // attribute to tell it what script we are customizing
+public class LevelScriptEditor : Editor
+{
+    public override void OnInspectorGUI() // called every time the inspector is drawn
+    {
+        LevelScript myTarget = (LevelScript)target; // cast the target (the script we are overriding) to type LevelScript
+
+        myTarget.experience = EditorGUILayout.IntField("Experience", myTarget.experience); // create an integer field named Experience which writes to the experience variable
+        EditorGUILayout.LabelField("Level", myTarget.Level.ToString()); // read only field representing the level
+    }
+}
