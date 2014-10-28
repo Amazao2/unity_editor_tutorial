5d5ca24f6549d630dfca177c7e379b8b8c6dc01c
Brandon McRae <bmcrae2@uwo.ca>
2014-10-28 17:59:52 -0400

    Custom Editor For My Dataset
    - adds Character Attributes and associated editor script
    - scene2 has a character where this can be viewed

---
+++ editor_tutorial/Assets/Editor/CharacterAttributesEditor.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CharacterAttributes))]
+class CharacterAttributesEditor : Editor
+{
+    // specify bounds for attributes
+    int attributeMax = 100;
+    int attributeMin = 0;
+
+    public override void OnInspectorGUI()
+    {
+        CharacterAttributes character = (CharacterAttributes)target;
+
+        // class
+        EditorGUILayout.EnumPopup("Class", character.characterClass, EditorStyles.popup);
+
+        // experience and level
+        character.experience = EditorGUILayout.IntField("Experience", character.experience);
+        EditorGUILayout.LabelField("Level", character.level.ToString());
+
+        // attributes
+        EditorGUILayout.PrefixLabel("Attributes:");
+        EditorGUI.indentLevel++;
+        character.stamina = EditorGUILayout.IntSlider(new GUIContent("Stamina"), character.stamina, attributeMin, attributeMax);
+        character.agility = EditorGUILayout.IntSlider(new GUIContent("Agility"), character.agility, attributeMin, attributeMax);
+        character.intelligence = EditorGUILayout.IntSlider(new GUIContent("Intelligence"), character.intelligence, attributeMin, attributeMax);
+
+        // resources (based on attributes)
+        EditorGUI.indentLevel--;
+        EditorGUILayout.PrefixLabel("Resources:");
+        EditorGUI.indentLevel++;
+        EditorGUILayout.LabelField("Health", character.maxHealth.ToString());
+        EditorGUILayout.LabelField("Energy", character.maxEnergy.ToString());
+        EditorGUILayout.LabelField("Mana", character.maxMana.ToString());
+
+    }
+    
+}
---
+++ editor_tutorial/Assets/Scripts/CharacterAttributes.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum CharacterClass { Thief, Wizard, Warrior }
+
+public class CharacterAttributes : MonoBehaviour {
+
+    // character type
+    public CharacterClass characterClass;
+
+    // character level is based on exp
+    public int experience;
+    public int level
+    {
+        get { return experience / 750; }
+    }
+
+    // main attributes
+    public int stamina;
+    public int agility;
+    public int intelligence;
+
+    // attributes based on main attributes
+    public int maxHealth
+    {
+        get { return stamina * 5;  }
+    }
+    public int maxMana
+    {
+        get { return intelligence * 10; }
+    }
+    public int maxEnergy
+    {
+        get { return agility * 15; }
+    }
+
+}
